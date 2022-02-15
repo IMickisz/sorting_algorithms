c375291522416fddcdd1b216ab4fe538b62cd08e
Ivan Mickisz <ivan.mickisz3@gmail.com>
2022-02-15 18:24:20 +0100

    changed for requirement

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,17 +12,16 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
-	if (size >= 2)
+	if (size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
 	{
-		for (i = 0; i < size - 1; i++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			for (j = 0; j < size - i - 1; j++)
+			if (array[j] > array[j + 1])
 			{
-				if (array[j] > array[j + 1])
-				{
-					swap(&array[j], &array[j + 1]);
-					print_array(array, size);
-				}
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
 			}
 		}
 	}
