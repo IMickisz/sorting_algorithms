287ddc73d7e548865844105f92715a259d73f309
Ivan Mickisz <ivan.mickisz3@gmail.com>
2022-02-15 16:49:30 +0100

    added prototype

--- sort.h
+++ sort.h
@@ -21,8 +21,12 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
 void bubble_sort(int *array, size_t size);
-void swap(int *a, int *b);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+
+void swap(int *a, int *b);
+int partition(int array[], int low, int high, size_t size);
+void quick_sort_array(int array[], int low, int high, size_t size);
+
 #endif
