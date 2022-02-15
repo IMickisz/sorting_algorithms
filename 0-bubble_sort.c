0adf82e3de51de94e67bce8d63e89360963ffaef
Ivan Mickisz <ivan.mickisz3@gmail.com>
2022-02-15 16:07:18 +0100

    added size limit

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,10 +1,10 @@
 #include "sort.h"
 
 /**
- * bubble_sort - function that sorts an array of integers in ascending order
- * using the Bubble sort algorithm
- * @array: the array to sort
- * @size: the size of the array
+ * bubble_sort - function that sorts an array of integers in ascending
+ * order using the Bubble sort algorithm
+ * @array: array to sort
+ * @size: size of the array
  * Return: Nothing
  */
 
@@ -12,13 +12,21 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
-	for (i = 0; i < size - 1; i++)
-		for (j = 0; j < size - i - 1; j++)
-			if (array[j] > array[j + 1])
+	if (size > 2)
+	{
+		for (i = 0; i < size - 1; i++)
+		{
+			for (j = 0; j < size - i - 1; j++)
 			{
-				swap(&array[j], &array[j + 1]);
-				print_array(array, size);
+				if (array[j] > array[j + 1])
+				{
+					swap(&array[j], &array[j + 1]);
+					print_array(array, size);
+				}
 			}
+		}
+	}
+	return;
 }
 
 /**
